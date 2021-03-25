b4d2ef300f8892d6c5c562b8355a9116b57c48de
cicerotcv <elm.tiago@gmail.com>
2021-03-25 00:35:54 -0300

    add models and types

--- search.py
+++ search.py
@@ -3,17 +3,18 @@ import json
 import re
 import sys
 import unicodedata
+from typing import List
 
-from helpers import load_file, remove_accents, timeit
+from helpers import City, load_file, remove_accents, timeit
 
 
 @timeit
 def search(query: str):
     query = remove_accents(query)
-    cities = json.loads(load_file("cities.json"))
+    cities = load_file("cities.json", City)
 
-    def find_match(city):
-        return re.search(query, remove_accents(city["name"]),
+    def find_match(city: City):
+        return re.search(query, remove_accents(city.name),
                          re.IGNORECASE | re.MULTILINE) != None
 
     res = filter(find_match, cities)
